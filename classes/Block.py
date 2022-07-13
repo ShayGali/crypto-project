16e7c8685c9d9e0b327c4960cd0325e9c9560fb0
ShayGali <shayali100@gmail.com>
2022-07-13 15:44:04 +0300

    add utilities folder

--- classes/Block.py
+++ classes/Block.py
@@ -12,20 +12,20 @@ from typing import Callable, List
 @dataclass
 class Block:
     prev_block_hash: str
-    index:int
-    messages:List[Message]
-    current_block_hash:str = dc.field(init=False)
-    time_added:datetime = dc.field(init=False)
-    nonce:int = dc.field(init=False)
+    index: int
+    messages: List[Message]
+    current_block_hash: str = dc.field(init=False)
+    time_added: datetime = dc.field(init=False)
+    nonce: int = dc.field(init=False)
 
     TOKEN_PRIZE = 3
 
-    def compute_block_header(self)->bytes:
+    def compute_block_header(self) -> bytes:
         block_str = \
-            get_fields_str(self.prev_block_hash,self.index,self.messages,self.time_added,self.nonce)
+            get_fields_str(self.prev_block_hash, self.index, self.messages, self.time_added, self.nonce)
         return block_str.encode()
 
-    def compute_block_hash(self)->str:
+    def compute_block_hash(self) -> str:
         """
         computes the hash of the block using SHA-256 algorithm
         :return: the hash in hexadecimal
@@ -33,6 +33,3 @@ class Block:
         block_hash = hashlib.sha256(self.compute_block_header()).hexdigest()
         self.current_block_hash = block_hash
         return block_hash
-
-
-
