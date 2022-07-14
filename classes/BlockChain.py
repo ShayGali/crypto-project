ed8d523b9ee5dd621c46f624c3de4c38a0c612a4
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-07-14 18:58:56 +0300

    Update BlockChain.py

--- classes/BlockChain.py
+++ classes/BlockChain.py
@@ -13,12 +13,28 @@ from typing import Callable, List
 
 @dataclass
 class BlockChain:
+    
+        # TODO: return reward to Miner - SHIR
+    def __init__(self,*transactions):
+        """
+        :param transactions: an unknown number of Transaction objects
+        """
+        self.transactions = list()
+        if transactions:
+            if all(isinstance(a_transaction,Transaction) for a_transaction in transactions):
+                self.transactions.extend(transactions)
+
+    def add_trans_to_block(self,transaction) -> None:
+        if isinstance(transaction, Transaction):
+            if len(self.transactions) > 0:
+                transaction.link_transactions(self.transactions[-1])
+            transaction.seal()
+            # check that there is no tempering between seal() and appending the transaction
+            transaction.validate_integrity()
+            self.transactions.append(transaction)
+    
     block_head: Block
     block_hash: str
-
-    # TODO: Create the method, return something to Miner - SHIR
-    def add_trans_to_block(self) -> None:
-        pass
 
     # TODO: Create the method - AVIAL
     def add_trans_to_queue(self, trans: Transaction) -> None:
