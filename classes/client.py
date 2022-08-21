7b1b561c7d4dd858d444f82ce4a20d4445a8b4cb
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-08-21 13:51:34 +0300

    updated 'private_key' attribute to private

--- classes/client.py
+++ classes/client.py
@@ -6,15 +6,14 @@ import rsa
 class Client:
     tokens: float
     address: rsa.PublicKey = dc.field(init=False)
-    private_key: rsa.PrivateKey = dc.field(init=False)
+    _private_key: rsa.PrivateKey = dc.field(init=False)
 
     def __post_init__(self):
-        self.address, self.private_key = rsa.newkeys(512)
+        self.address, self._private_key = rsa.newkeys(512)
 
     # TODO: add validations
-    def validate_enough_tokens(self, amount)->bool:
+    def validate_enough_tokens(self, amount):
         if amount < self.tokens:
             return True
         return False
-
 
